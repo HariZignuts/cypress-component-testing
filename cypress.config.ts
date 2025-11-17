d3eec32ff29d2ddf180e2936e5a53c2c61f510bb
Hari Malam <harim@zignuts.com>
2025-11-17 18:39:40 +0530

    fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,13 +13,13 @@ const sharedViteConfig = {
   root: __dirname,
   resolve: {
     alias: {
-      "@/components": path.resolve(__dirname, "src/components"),
-      "@/core": path.resolve(__dirname, "src/core"),
-      "@/types": path.resolve(__dirname, "src/types"),
-      "@/assets": path.resolve(__dirname, "src/assets"),
-      "@pom": path.resolve(__dirname, "cypress/support/pom"),
-      "@factories": path.resolve(__dirname, "cypress/support/factories"),
-      "@fixtures": path.resolve(__dirname, "cypress/fixtures"),
+      "@/components/*": path.resolve(__dirname, "src/components/*"),
+      "@/core/*": path.resolve(__dirname, "src/core/*"),
+      "@/types/*": path.resolve(__dirname, "src/types/*"),
+      "@/assets/*": path.resolve(__dirname, "src/assets/*"),
+      "@pom/*": path.resolve(__dirname, "cypress/support/pom/*"),
+      "@factories/*": path.resolve(__dirname, "cypress/support/factories/*"),
+      "@fixtures/*": path.resolve(__dirname, "cypress/fixtures/*"),
     },
   },
 };
