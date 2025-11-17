ba68fcbc814ca4f5f86e341ef1fb42c0521cba91
Hari Malam <harim@zignuts.com>
2025-11-17 18:22:30 +0530

    remove unwanted

--- cypress/components/Button.cy.tsx
+++ cypress/components/Button.cy.tsx
@@ -2,28 +2,28 @@ import { Button } from "@/components/Button";
 import { buttonPOM } from "@pom/index";
 
 describe("Button.cy.tsx", () => {
-  it("should mount and be visible", () => {
-    // 1. This is the new command!
-    // We "mount" the component instead of "visiting" a page.
-    cy.mount(<Button onClick={() => {}}>Click Me</Button>);
+  // it("should mount and be visible", () => {
+  //   // 1. This is the new command!
+  //   // We "mount" the component instead of "visiting" a page.
+  //   cy.mount(<Button onClick={() => {}}>Click Me</Button>);
 
-    // 2. From here, all your normal Cypress commands work
-    buttonPOM.getButton().should("be.visible").and("contain.text", "Click Me");
-  });
+  //   // 2. From here, all your normal Cypress commands work
+  //   buttonPOM.getButton().should("be.visible").and("contain.text", "Click Me");
+  // });
 
-  it("should be disabled when disabled prop is true", () => {
-    cy.mount(
-      <Button onClick={() => {}} disabled>
-        Disabled Button
-      </Button>
-    );
+  // it("should be disabled when disabled prop is true", () => {
+  //   cy.mount(
+  //     <Button onClick={() => {}} disabled>
+  //       Disabled Button
+  //     </Button>
+  //   );
 
-    buttonPOM.getButton().should("be.disabled");
+  //   buttonPOM.getButton().should("be.disabled");
 
-    buttonPOM
-      .getButton()
-      .should("have.css", "background-color", "rgb(128, 128, 128)"); // grey
-  });
+  //   buttonPOM
+  //     .getButton()
+  //     .should("have.css", "background-color", "rgb(128, 128, 128)"); // grey
+  // });
 
   it("should call the onClick handler when clicked", () => {
     const onClickSpy = cy.spy().as("onClickSpy");
